1b13bf11543e2f01f9dba067b72e72139cdac31a
yakann <42899073+yakann@users.noreply.github.com>
2020-08-08 17:33:58 +0300

    tasarımsal değişiklikler yapıldı

--- src/BookHome/settings.py
+++ src/BookHome/settings.py
@@ -124,4 +124,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/Static/'
+
+BOOTSTRAP4 = {
+    'include_jquery': True,
+}
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "Static"),
+    '/var/www/Static/',
+]
